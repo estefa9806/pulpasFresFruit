af04ba721b5a5608da59a1813881c6dbf5c05570
Sebastian Rojo Londoño <sebastian.rojo@multienlace.com.co>
2019-01-30 17:00:53 -0500

    Async await functions

--- server/src/routes/orders.js
+++ server/src/routes/orders.js
@@ -3,22 +3,21 @@ const router = Router();
 const mysqlConnection = require('../database');
 
 // Get all
-router.get('/', (req, res) => {
-    mysqlConnection.query('SELECT * FROM ff_pedidos', (err, rows, fields) => {
+router.get('/', async (req, res) => {
+    await mysqlConnection.query('SELECT * FROM ff_pedidos', (err, rows, fields) => {
         if (!err) {
             res.json(rows);
         } else {
             console.log(err);
         }
     });
-    //res.json(data);
 });
 
 // Get one
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
     const { id } = req.params;
     
-    mysqlConnection.query('SELECT * FROM ff_pedidos where id_pedido = ?', [id], (err, rows, fields) => {
+    await mysqlConnection.query('SELECT * FROM ff_pedidos where id_pedido = ?', [id], (err, rows, fields) => {
         if (!err) {
             res.json(rows[0]);
         } else {
@@ -28,10 +27,10 @@ router.get('/:id', (req, res) => {
 });
 
 // Insert
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     const { observacion } = req.body;
 
-    mysqlConnection.query('INSERT INTO ff_pedidos SET() VALUES(?)', [observacion], (err, rows, fields) => {
+    await mysqlConnection.query('INSERT INTO ff_pedidos SET() VALUES(?)', [observacion], (err, rows, fields) => {
         if (!err) {
             res.json({status: 'Order saved'});
         } else {
@@ -41,11 +40,11 @@ router.post('/', (req, res) => {
 });
 
 // Update
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
     const { observacion } = req.body;
     const { id } = req.params;
     
-    mysqlConnection.query('UPDATE ff_pedidos SET observacion = ? WHERE id_pedido = ?', [observacion, id], (err, rows, fields) => {
+    await mysqlConnection.query('UPDATE ff_pedidos SET observacion = ? WHERE id_pedido = ?', [observacion, id], (err, rows, fields) => {
         if (!err) {
             res.json({status: 'Order updated'});
         } else {
@@ -55,10 +54,10 @@ router.put('/:id', (req, res) => {
 });
 
 // Delete
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
     const { id } = req.params;
     
-    mysqlConnection.query('DELETE FROM ff_pedidos WHERE id_pedido = ?', [id], (err, rows, fields) => {
+    await mysqlConnection.query('DELETE FROM ff_pedidos WHERE id_pedido = ?', [id], (err, rows, fields) => {
         if (!err) {
             res.json({status: 'Order deleted'});
         } else {
